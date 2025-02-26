f921615b62628c49abc1bd825f91899e8a77f9d0
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2025-02-26 13:23:22 +0100

    dev change

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dsgni1uqssg"
+    "revision": "0.g3q3liaifu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
