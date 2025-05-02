0e3eb855b18c4d5bda685fb732db2434e17368d7
ISHOWO ASMAA KHADIJAH <asmaaishowo2912@gmail.com>
2025-05-02 15:12:25 +0100

    requirement.txt

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.baoi4mt7g9"
+    "revision": "0.f46soogiua"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
